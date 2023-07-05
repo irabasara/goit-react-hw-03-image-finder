2a328363c6aca424f31c3de5704e939ecc88e331
irabasara <125157770+irabasara@users.noreply.github.com>
2023-07-05 20:24:40 +0300

    Update pixabayAPI.js

--- src/utils/pixabayAPI.js
+++ src/utils/pixabayAPI.js
@@ -3,7 +3,7 @@ const API_KEY = '36518003-e50cc2d75c5794a64cca810ae';
 
 const getGallery = async (searchText, page) => {
   const response = await fetch(
-    `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&q=${searchText}&page=${page}&image_type=photo&orientation=horizontal&per_page=200`
+    `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&q=${searchText}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
   );
   if (response.ok) {
     return response.json();
